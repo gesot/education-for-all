41216361b1880dc2e5708dc9626512abe059df32
emilimicha <emilimicha@csd.auth.gr>
2021-06-05 23:00:58 +0300

    Added films tab

--- information.php
+++ information.php
@@ -1,146 +1,143 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 
-  <head>
-    <meta charset="utf-8">
-    <title>Education For All- Information</title>
-    <link rel="stylesheet" href="index.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;600;700&display=swap"
-      rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="information.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  </head>
+<head>
 
-  <body>
-    <section class="foo">
-    <?php include('nav/navbar.php');?>
-      <!-- START OF INFORMATION PAGE CODE -->
-      <div class="d-flex flex-column justify-content-center align-items-center text-center infostart text-wrap">
-        <div class="row justify-content-center">
-          <h1 class="imgtitle"><span id="titlecrl">Education</span> Around The World</h1>
-        </div>
-        <div class="row align-items-center justify-content-center">
-          <h2 class="wht">An Overview Of the Current State of Education Around The World </h2>
-        </div>
-        <div class="row align-items-center justify-content-center" style="margin-top: 1em;">
-          <a href="#information" class="btn btn-outline-light infobtn btn-lg" role="button"
-            data-bs-toggle="button">Learn More</a>
-        </div>
+  <?php include('nav/navbar.php'); ?>
+  <meta charset="utf-8">
+  <title>Education For All- Information</title>
+  <link rel="stylesheet" href="index.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;600;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="information.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+  <section class="foo">
+    <!-- START OF INFORMATION PAGE CODE -->
+    <div class="d-flex flex-column justify-content-center align-items-center text-center infostart text-wrap">
+      <div class="row justify-content-center">
+        <h1 class="imgtitle"><span id="titlecrl">Education</span> Around The World</h1>
       </div>
-    </section>
-    <div class="container" id="information">
-      <!--ΕΙΚΟΝΑ ΜΕ ΚΕΙΜΕΝΟ-->
-      <div class="row">
-        <div class="col-lg-12" style="margin-top: 50px;">
-          <h2>Education Around The World</h2>
-          <p> In recent years, more children are enrolled in school than ever before, majority of countries try to
-            encourage children to advance past primary school to continue their education. Yet, even if the number of
-            students who quit after primary school is very low, the actual number of students who attend secondary
-            school and above varies greatly from country to country.<br><br> Bellow you will see various facts regarding
-            the state of education around the world and factors such as gender and the state of the country itself. </p>
-        </div>
+      <div class="row align-items-center justify-content-center">
+        <h2 class="wht">An Overview Of the Current State of Education Around The World </h2>
       </div>
-      <div class="row justify-content-start" style='margin-top:50px;'>
-        <div class="col-lg-6 text-center boxbox">
-          <img class='img-fluid' src="images/gender+inequality.jpeg" alt="Gender inequality photo">
-        </div>
-        <div class="col-lg-6 boxbox">
-          <h3>Gender Inequality</h3>
-          <p>An estimate of 129 millions girls worldwide remain out of school and face multiple barriers to education.
-            Globally, 9 in 10 girls complete their primary education, but only 3 in 4 complete their lower secondary
-            education. In low-income countries, less than two thirds of girls complete their primary education, and only
-            1 in 3 completes lower secondary school. <br><br>This problem occurs most frequently in the Arab States, in
-            central Asia and in Southern and Western Asia and is principally explained by the cultural and traditional
-            privileged treatment given to males. Girls are destined to work in the family home, whereas boys are
-            entitled to receive an education. </p>
-        </div>
-      </div>
-      <div class="row" style="margin-top:100px;">
-        <!--ΚΕΙΜΕΝΟ ΜΕ ΕΙΚΟΝΑ-->
-        <div class="col-lg-6 boxbox ">
-          <h3>Illiteracy </h3>
-          <p>According to World Atlas, the sub-saharan region has the highest illiteracy rate in the world. An estimated
-            40.1% of the population is illiterate. Fourteen out of the 22 countries in the world with literacy rates
-            below 60% are in Sub-Saharan Africa and only only 64% of primary and 50% of secondary school teachers have
-            the minimum required training, and this proportion has been declining since 2000. <br><br>In South-Asia and
-            Arab States the illiteracy rate is 31.3% and 26.6% respectively, whereas in Europe and Central Asia it was
-            1.8% in 2016. </p>
-        </div>
-        <div class="col-lg-6 boxbox">
-          <img class='img-fluid' src="images/books.webp" alt="books">
-        </div>
-      </div>
-      <div class="wrapper">
-        <div class="divider div-transparent"></div>
-      </div>
-      <div class="row dyk justify-content-center align-items-center">
-        <span>Did you know <span id="qm">?</span></span>
-        <p> Each year of education reduces the risk of conflict by around 20%.</p>
-      </div>
-      <div class="wrapper">
-        <div class="divider div-transparent"></div>
-      </div>
-      <div class="row justify-content-start" style='margin-top:30px;'>
-        <div class="col-lg-6 text-center ">
-          <img class='img-fluid imgincl' src="images/inclus2.webp" alt="inclusive">
-        </div>
-        <div class="col-lg-6 boxbox">
-          <h3>Inclusivity</h3>
-          <p>Inclusivity means that all children, including those with disabilities and speakers of minority languages,
-            get the chance to go to school, be in a classroom. <br><br>Yet, children with disabilities are often
-            overlooked and are more likely to stay out of school.</p>
-          <p class="incl"> In low and lower-middle income countries, around 40% of children with disabilities are out of
-            school at primary level and 55% at lower secondary level <br><br>Approximately one billion people in the
-            world are living with a disability, with at least 1 in 10 being children and 80% living in lower-income
-            countries. <br><br>The literacy rate for adults with disabilities is 3%. For women with disabilities the
-            literacy rate is even lower, at 1% </p>
-          <p>Inclusive education is the most effective way to give all children a fair chance to go to school, learn and
-            develop the skills they need to thrive.</p>
-        </div>
-      </div>
-      <div class="row rowend" style="margin-top:100px;">
-        <!--ΚΕΙΜΕΝΟ ΜΕ ΕΙΚΟΝΑ-->
-        <div class="col-lg-6 ">
-          <h3>Low-Income Countries </h3>
-          <p> In an ideal world, primary education would be universal and publicly financed, and all children would be
-            able to attend school regardless of their parents' ability or willingness to pay. <br><br>Despite
-            considerable progress over the past two decades, however, school attendance in the world's poorest countries
-            is by no means universal.</p>
-          <p class="incl"> 91% of primary school-age children in low-income countries will not achieve minimum
-            proficiency levels in reading and the rate is 87% in math compared to 5% and 8% respectively in high-income
-            countries. <br><br> 93% of secondary school-age adolescents in low-income countries will not achieve minimum
-            proficiency levels in reading compared to 27% in high-income countries. Source: UIS Fact Sheet 46, p.11
-            <br><br> Low- and middle-income countries spend 2% of their GDP each year on education costs that do not
-            lead to learning. <br><br> Approximately 1 in 4 young people in low and lower-middle income countries is
-            illiterate. <br><br>An estimated 53% of children in low- and middle-income countries cannot read
-            proficiently by age 10. 
-          </p>
-        </div>
-        <div class="col-lg-6 boxbox">
-          <img class='img-fluid' src="images/lowincome2.webp" alt="low income photo">
-        </div>
-      </div>
-      <div class="row rowend">
-        <p>Sources: <a href="https://ourworldindata.org/global-education">Our World in Data</a>, <a
-            href="https://www.unicef.org/education">Unicef</a>, <a
-            href="https://restavekfreedom.org/2018/06/14/schools-around-the-world-how-does-education-differ-from-country-to-country/">Restavek
-            Freedom</a>, <a href="https://www.globalpartnership.org/results/education-data-highlights">Global Partnership </a>, <a href="https://www.humanium.org/en/right-to-education/"> Humanium</a> </p>
+      <div class="row align-items-center justify-content-center" style="margin-top: 1em;">
+        <a href="#information" class="btn btn-outline-light infobtn btn-lg" role="button" data-bs-toggle="button">Learn More</a>
       </div>
     </div>
-    <!-- END OF INFORMATION PAGE CODE -->
-    <!--START OF FOOTER-------->
-    <footer>
-    <?php include('nav/footer.php');?>
-    </footer>
-    <!--END OF FOOTER-->
-    <!--START OF <TOP> BUTTON-->
-    <a href="#"><i class="gotopbtn" href="#"><i class="fa fa-arrow-circle-up"></i></i></a>
-    <!--END OF TOP BUTTON-->
-  </body>
+  </section>
+  <div class="container" id="information">
+    <!--ΕΙΚΟΝΑ ΜΕ ΚΕΙΜΕΝΟ-->
+    <div class="row">
+      <div class="col-lg-12" style="margin-top: 50px;">
+        <h2>Education Around The World</h2>
+        <p> In recent years, more children are enrolled in school than ever before, majority of countries try to
+          encourage children to advance past primary school to continue their education. Yet, even if the number of
+          students who quit after primary school is very low, the actual number of students who attend secondary
+          school and above varies greatly from country to country.<br><br> Bellow you will see various facts regarding
+          the state of education around the world and factors such as gender and the state of the country itself. </p>
+      </div>
+    </div>
+    <div class="row justify-content-start" style='margin-top:50px;'>
+      <div class="col-lg-6 text-center boxbox">
+        <img class='img-fluid' src="images/gender+inequality.jpeg" alt="Gender inequality photo">
+      </div>
+      <div class="col-lg-6 boxbox">
+        <h3>Gender Inequality</h3>
+        <p>An estimate of 129 millions girls worldwide remain out of school and face multiple barriers to education.
+          Globally, 9 in 10 girls complete their primary education, but only 3 in 4 complete their lower secondary
+          education. In low-income countries, less than two thirds of girls complete their primary education, and only
+          1 in 3 completes lower secondary school. <br><br>This problem occurs most frequently in the Arab States, in
+          central Asia and in Southern and Western Asia and is principally explained by the cultural and traditional
+          privileged treatment given to males. Girls are destined to work in the family home, whereas boys are
+          entitled to receive an education. </p>
+      </div>
+    </div>
+    <div class="row" style="margin-top:100px;">
+      <!--ΚΕΙΜΕΝΟ ΜΕ ΕΙΚΟΝΑ-->
+      <div class="col-lg-6 boxbox ">
+        <h3>Illiteracy </h3>
+        <p>According to World Atlas, the sub-saharan region has the highest illiteracy rate in the world. An estimated
+          40.1% of the population is illiterate. Fourteen out of the 22 countries in the world with literacy rates
+          below 60% are in Sub-Saharan Africa and only only 64% of primary and 50% of secondary school teachers have
+          the minimum required training, and this proportion has been declining since 2000. <br><br>In South-Asia and
+          Arab States the illiteracy rate is 31.3% and 26.6% respectively, whereas in Europe and Central Asia it was
+          1.8% in 2016. </p>
+      </div>
+      <div class="col-lg-6 boxbox">
+        <img class='img-fluid' src="images/books.webp" alt="books">
+      </div>
+    </div>
+    <div class="wrapper">
+      <div class="divider div-transparent"></div>
+    </div>
+    <div class="row dyk justify-content-center align-items-center">
+      <span>Did you know <span id="qm">?</span></span>
+      <p> Each year of education reduces the risk of conflict by around 20%.</p>
+    </div>
+    <div class="wrapper">
+      <div class="divider div-transparent"></div>
+    </div>
+    <div class="row justify-content-start" style='margin-top:30px;'>
+      <div class="col-lg-6 text-center ">
+        <img class='img-fluid imgincl' src="images/inclus2.webp" alt="inclusive">
+      </div>
+      <div class="col-lg-6 boxbox">
+        <h3>Inclusivity</h3>
+        <p>Inclusivity means that all children, including those with disabilities and speakers of minority languages,
+          get the chance to go to school, be in a classroom. <br><br>Yet, children with disabilities are often
+          overlooked and are more likely to stay out of school.</p>
+        <p class="incl"> In low and lower-middle income countries, around 40% of children with disabilities are out of
+          school at primary level and 55% at lower secondary level <br><br>Approximately one billion people in the
+          world are living with a disability, with at least 1 in 10 being children and 80% living in lower-income
+          countries. <br><br>The literacy rate for adults with disabilities is 3%. For women with disabilities the
+          literacy rate is even lower, at 1% </p>
+        <p>Inclusive education is the most effective way to give all children a fair chance to go to school, learn and
+          develop the skills they need to thrive.</p>
+      </div>
+    </div>
+    <div class="row rowend" style="margin-top:100px;">
+      <!--ΚΕΙΜΕΝΟ ΜΕ ΕΙΚΟΝΑ-->
+      <div class="col-lg-6 ">
+        <h3>Low-Income Countries </h3>
+        <p> In an ideal world, primary education would be universal and publicly financed, and all children would be
+          able to attend school regardless of their parents' ability or willingness to pay. <br><br>Despite
+          considerable progress over the past two decades, however, school attendance in the world's poorest countries
+          is by no means universal.</p>
+        <p class="incl"> 91% of primary school-age children in low-income countries will not achieve minimum
+          proficiency levels in reading and the rate is 87% in math compared to 5% and 8% respectively in high-income
+          countries. <br><br> 93% of secondary school-age adolescents in low-income countries will not achieve minimum
+          proficiency levels in reading compared to 27% in high-income countries. Source: UIS Fact Sheet 46, p.11
+          <br><br> Low- and middle-income countries spend 2% of their GDP each year on education costs that do not
+          lead to learning. <br><br> Approximately 1 in 4 young people in low and lower-middle income countries is
+          illiterate. <br><br>An estimated 53% of children in low- and middle-income countries cannot read
+          proficiently by age 10.
+        </p>
+      </div>
+      <div class="col-lg-6 boxbox">
+        <img class='img-fluid' src="images/lowincome2.webp" alt="low income photo">
+      </div>
+    </div>
+    <div class="row rowend">
+      <p>Sources: <a href="https://ourworldindata.org/global-education">Our World in Data</a>, <a href="https://www.unicef.org/education">Unicef</a>, <a href="https://restavekfreedom.org/2018/06/14/schools-around-the-world-how-does-education-differ-from-country-to-country/">Restavek
+          Freedom</a>, <a href="https://www.globalpartnership.org/results/education-data-highlights">Global Partnership </a>, <a href="https://www.humanium.org/en/right-to-education/"> Humanium</a> </p>
+    </div>
+  </div>
+  <!-- END OF INFORMATION PAGE CODE -->
+  <!--START OF FOOTER-------->
+  <footer>
+    <?php include('nav/footer.php'); ?>
+  </footer>
+  <!--END OF FOOTER-->
+  <!--START OF <TOP> BUTTON-->
+  <a href="#"><i class="gotopbtn" href="#"><i class="fa fa-arrow-circle-up"></i></i></a>
+  <!--END OF TOP BUTTON-->
+</body>
 
 </html>
