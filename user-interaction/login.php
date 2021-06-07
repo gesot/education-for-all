789dde490372dcb59179013e75138f3353ab9c3a
Efthymia Sarri <efthymvs@csd.auth.gr>
2021-06-07 22:08:44 +0300

    login-with-*

--- user-interaction/login.php
+++ user-interaction/login.php
@@ -7,9 +7,10 @@
         $email = trim($_POST['email']);
         $password = trim($_POST['password']);
         //if email exists fetch encrypted password from database
-        $checkPass = "SELECT 'password' FROM `user` WHERE email='$email' LIMIT 1";
+        $checkPass = "SELECT * FROM `user` WHERE email='$email' LIMIT 1";
         $checkPassQuery = mysqli_query($connection, $checkPass);
         if(mysqli_num_rows($checkPassQuery) == 1){
+            echo 'email exists ';
             $result = mysqli_fetch_assoc($checkPassQuery);
             $hashedPassword = $result['password'];
             $verify = password_verify($password, $hashedPassword);
