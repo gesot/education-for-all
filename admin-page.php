3f29a3ec1eeed36672200633bc069e072be007cd
Efthymia Sarri <efthymvs@csd.auth.gr>
2021-06-09 13:25:56 +0300

    dont-push-this

--- admin-page.php
+++ admin-page.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <?php include('nav/navbar.php');?>
     <meta charset="utf-8" />
     <title>Education For All</title>
-    <link rel="stylesheet" href="admin-page.css" />
     <link rel="stylesheet" href="index.css" />
+    
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="preconnect" href="https://fonts.gstatic.com" />
     <link
@@ -26,35 +27,37 @@
     />
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="admin-page.css" />
   </head>
 <body>
     <section class = "foo">
-        <?php include('nav/navbar.php');?>
+        
     </section>
     <section class = "top-part">
-        <div class = "container-fluid" style = "background-color:white; margin-top: 2em;">
-            <div class="row" style="">
-                <div class="col-lg-1" style = "background-color:white;margin-top:3em">
+        <div class = "container-fluid" style = "background-color: #FFFAF0; margin-top: 2em;">
+            <div class="row" style="background-color: #FFFAF0">
+                <div class="col-lg-1" style = "background-color:#FFFAF0;margin-top:3em">
                 </div>
-                <div class="col-lg-3" style = "background-color:white;margin-top:3em">
-                    <div class = "box" style = "border-radius: 5%;margin: 0 auto;width: 23em; height: 12em; background-color: orange;">
+                <div class="col-lg-3" style = "background-color:#FFFAF0;margin-top:3em">
+                    <div class = "box" style = "border-radius: 5%;margin: 0 auto;width: 23em; height: 12em; background-color: #ADD8E6;">
                     <img src="images/icons/user.png" style="position:relative; width: 5em; margin-top: 3em;">
 
                     </div>
                 </div>
 
-                <div class="col-lg-4" style = "background-color: white;margin-top:3em">
-                <div class = "box" style = "margin: 0 auto;width: 23em; height: 12em; background-color: orange;">      
+                <div class="col-lg-4" style = "background-color: #FFFAF0;margin-top:3em">
+                <div class = "box" style = "margin: 0 auto;width: 23em; height: 12em; background-color: #ADD8E6;">      
                 </div>
                 </div>
            
-                <div class="col-lg-3" style = "background-color: white;margin-top:3em">
-                <div class = "box" style = "margin: 0 auto;width: 23em; height: 12em; background-color: orange;">
+                <div class="col-lg-3" style = "background-color: #FFFAF0;margin-top:3em">
+                <div class = "box" style = "margin: 0 auto;width: 23em; height: 12em; background-color: #ADD8E6;">
                 </div>
                 </div>
             </div>
         </div>
-        <div class = "container-fluid" style = "background-color:purple; margin-top: 2em;">
+    </section>
+    <div class = "container-fluid" style = "background-color:purple; margin-top: 2em;">
             <div class="row" style = "background-color:purple;">
                 <div class="col-md-7" style="background-color: red;">
                     <div class="user-box" style="overflow-x:auto;">
@@ -63,9 +66,6 @@
                                 <th>Name</th>
                                 <th>Email</th>
                                 <th>Comment</th>
-                                <th>Status</th>
-                                <th>Status</th>
-                                <th>Status</th>
                                 <th>Status</th>
                             </tr>
                             <tr>
@@ -97,7 +97,6 @@
                 </div>
             </div>
         </div>
-    </section>
 </body>
     
 </html>
