9a99ca04411067dd8659deb5c781077df91b1015
Efthymia Sarri <efthymvs@csd.auth.gr>
2021-06-11 01:22:04 +0300

    improved user table

--- user-interaction/user-table.php
+++ user-interaction/user-table.php
@@ -5,26 +5,22 @@
               ORDER BY `reg_date` DESC  ";
     $execQuery = mysqli_query($connection, $query);
     if(mysqli_num_rows($execQuery) > 0){
-        echo " <thead>
+        echo " <table class='content-table' style='margin-left: 50px; margin-bottom: 100px;'>
+        <thead>
         <tr>
             <th>ID</th>
             <th>Username</th>
             <th>Email</th>
         </tr>
-    </thead>";
+        </thead><tbody>";
         while ($result = mysqli_fetch_array($execQuery, MYSQLI_NUM)) {
             
-            //printf("username: %s  email: %s", $result[0], $result[1]);
-            echo "<tbody>
-            <tr>
-                <td>$result[0]</td>
-                <td>$result[1]</td>
-                <td>$result[2]</td>
-            </tr>
-            </tr>
-            ";
-            //echo "</br>";
+            echo "<tr>
+                    <td>$result[0]</td>
+                    <td>$result[1]</td>
+                    <td>$result[2]</td>
+                 </tr>";
         }
-        echo "</tbody>";
+        echo "</tbody> </table>";
     }
 ?>
