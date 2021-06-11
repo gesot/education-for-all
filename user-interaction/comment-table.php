3e109ef60c5d68f807a826692aa686377973f72c
Efthymia Sarri <efthymvs@csd.auth.gr>
2021-06-11 15:10:22 +0300

    comments/dont push

--- user-interaction/comment-table.php
+++ user-interaction/comment-table.php
@@ -1,11 +1,13 @@
-<?php
-    
-    $connection = mysqli_connect('localhost', 'pspi', 'pspi2021', 'users');
-    $query = "SELECT `id`,`page_id`,`name`,`content` FROM `comments` 
-              ORDER BY `submit_date` DESC  ";
-    $execQuery = mysqli_query($connection, $query);
-    if(mysqli_num_rows($execQuery) > 0){
-        echo " <table class='comment-table' style='margin-left: 50px; margin-bottom: 100px;'>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <table class='comment-table' style='margin-left: 50px; margin-bottom: 100px;'>
         <thead>
         <tr>
             <th>ID</th>
@@ -14,17 +16,26 @@
             <th>Comment</th>
             <th>Manage</th>
         </tr>
-        </thead><tbody>";
-        while ($result = mysqli_fetch_array($execQuery, MYSQLI_NUM)) {
-            
-            echo "<tr>
-                    <td>$result[0]</td>
-                    <td>$result[1]</td>
-                    <td>$result[2]</td>
-                    <td>$result[3]</td>
-                    <td><a href='delete-comment.php?id=<?php echo $result[0]; ?>'>Delete</a></td>
-                 </tr>";
-        }
-        echo "</tbody> </table>";
-    }
+        </thead><tbody>
+        <?php
+        $connection = mysqli_connect('localhost', 'pspi', 'pspi2021', 'users');
+        $query = "SELECT `id`,`page_id`,`name`,`content` FROM `comments` 
+              ORDER BY `submit_date` DESC  ";
+        $execQuery = mysqli_query($connection, $query);
+        if(mysqli_num_rows($execQuery) > 0){
+            while ($result = mysqli_fetch_array($execQuery)) { ?>
+                    <tr>
+                        <td> <?php echo $result[0] ?> </td>
+                        <td> <?php echo $result[1] ?> </td>
+                        <td> <?php echo $result[2] ?> </td>
+                        <td> <?php echo $result[3] ?> </td>
+                        <td><a href="user-interaction\delete-comment.php?id=<?php echo $result[0]; ?>">Delete</a></td>
+                    </tr>
+          <?php  } ?>
+            </tbody> </table>
+<?php    }
 ?>
+</body>
+</html>
+
+
--- user-interaction/delete-comment.php
+++ user-interaction/delete-comment.php
@@ -1,15 +1,34 @@
 <?php
-    //session_start();
-    $connection = mysqli_connect('localhost', 'pspi', 'pspi2021', 'users');
-    $id = $_GET['id'];
-    $deleteQuery = "DELETE FROM `comments` where `id` =  `$id`";
-    $delete = mysqli_query($connection, $deleteQuery);
-        
-    if($del)
-    {
-        mysqli_close($db); // Close connection
-        header("location:all_records.php"); // redirects to all records page
-        exit;	
+    function deleteComments(&$array){
+        $parentIdQuery =  "SELECT  `id` FROM  `comments` WHERE  `parent_id` =  $comid "; //
+        $parentId= mysqli_query($connection, $parentIdQuery);
+        if( mysqli_fetch_array($parentId)== false){
+            exit;
+        } else {
+            $result = mysqli_fetch_array($parentId);
+            foreach ($result as &$value){
+                deleteComment(mysqli_fetch_array($parentId)); 
+            }
+
+        }
+        $del = mysqli_query($connection, "DELETE FROM `comments` where `id` = $value " );
     }
-el
+    //session_start();
+        $connection = mysqli_connect('localhost', 'pspi', 'pspi2021', 'users');
+        $comid=$_GET['id'];
+        $parentIdQuery =  "SELECT  `id` FROM  `comments` WHERE  `parent_id` =  $comid "; //
+        $parentId= mysqli_query($connection, $parentIdQuery);
+        $result =  mysqli_fetch_array($parentId);
+        foreach ($result as &$value){
+            $del = mysqli_query($connection, "DELETE FROM `comments` where `id` = $value " );
+        }
+        $deleteQuery = "DELETE FROM `comments` where `id` = $comid";
+        $delete = mysqli_query($connection, $deleteQuery);
+        if($delete)
+        {
+            mysqli_close($connection); // Close connection
+            header('Location: /admin-page.php'); 
+            	
+        }
+    
 ?>
