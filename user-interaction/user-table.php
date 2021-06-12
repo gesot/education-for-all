728447f5b44f6f619cafcc6f3440d33f22b5243f
Efthymia Sarri <efthymvs@csd.auth.gr>
2021-06-12 23:12:25 +0300

    user table changes

--- user-interaction/user-table.php
+++ user-interaction/user-table.php
@@ -1,26 +1,30 @@
 <?php
-    
     $connection = mysqli_connect('localhost', 'pspi', 'pspi2021', 'users');
     $query = "SELECT `id`,`username`,`email` FROM `user` 
               ORDER BY `reg_date` DESC  ";
     $execQuery = mysqli_query($connection, $query);
-    if(mysqli_num_rows($execQuery) > 0){
-        echo " <table class='content-table' style='margin-left: 50px; margin-bottom: 100px;'>
+    if(mysqli_num_rows($execQuery) > 0){ ?>
+    
+        <table class='content-table' style='margin-left: 50px; margin-bottom: 100px;'>
         <thead>
         <tr>
             <th>ID</th>
             <th>Username</th>
             <th>Email</th>
+            <th>Manage</th>
         </tr>
-        </thead><tbody>";
-        while ($result = mysqli_fetch_array($execQuery, MYSQLI_NUM)) {
+        </thead><tbody> <?php
+        while ($result = mysqli_fetch_array($execQuery, MYSQLI_NUM)) { ?>
             
-            echo "<tr>
-                    <td>$result[0]</td>
-                    <td>$result[1]</td>
-                    <td>$result[2]</td>
-                 </tr>";
-        }
-        echo "</tbody> </table>";
-    }
+                <tr>
+                    <td><?php echo $result[0] ?></td>
+                    <td><?php echo $result[1] ?></td>
+                    <td><?php echo $result[2] ?></td>
+                    <td><a href="user-interaction\set-admin.php?userID=<?php echo $result[0]; ?>">Make Admin</a> | 
+                        <a href="user-interaction\delete-user.php?userID=<?php echo $result[0]; ?>">Delete</a>
+                    </td>
+                 </tr>
+  <?php      } ?>
+        </tbody> </table>
+<?php    }
 ?>
